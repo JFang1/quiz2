1a57e1616115a380b934655aa55c757b41588674
JFang1 <jmf133@pitt.edu>
2016-11-10 21:08:31 -0500

    Fixes style change error

--- js/quiz2.js
+++ js/quiz2.js
@@ -1,5 +1,5 @@
 // write jQuery or vanilla javascript to do the following:
-//  - loop through the list items in the <ol> and change their text and color to be different from what it is now
+// x - loop through the list items in the <ol> and change their text and color to be different from what it is now
 //  - use js and html to create at least one custom accordion (by yourself, no copy paste)
 // x - create a button in the html that, once clicked, will fire off the function described in the next buller point
 // x - write a function that will fade out all of the html inside the body tag and replace it with a custom goodbye message when you click the button
@@ -13,9 +13,11 @@
 	});
 
 	$(function() {
-		var olItems = document.getElementsByTagName("ol")
-		olItems.style.color = "red";
-		olItems.style.font = "Times";
+		var olItems = document.getElementsByTagName("ol");
+		for (var i = 0; i < olItems.length; i++) {
+			olItems[i].style.color = "red";
+			olItems[i].style.font = "Times";
+		}
 	});
 
 	$('input').on('click', function(e) {
